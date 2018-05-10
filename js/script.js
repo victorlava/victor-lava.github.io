193c8d033a8ab12227c013a146293842818b15e9
Victor Lava <hello@victorlava.com>
2018-05-10 16:19:58 +0300

    removed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 'use strict';
-var clickCounter = 0;
 
 /* Tasks:
 *  1. Paspaudus ant navigacijos linku jie turi likti "active"
@@ -34,63 +33,6 @@ var clickCounter = 0;
 
 $(document).ready(function() {
 
-    // 1.
-    $('.navigation li').on('click', function() {
-        $('.navigation li').removeClass('active');
-        $(this).addClass('active');
-    });
-
-    // 2.
-    $("#js-scroll").on('click', function() {
-        $('html, body').animate({
-            scrollTop: $(".team").offset().top
-        }, 1000);
-    });
-
-    // 3.
-    $('.member i').on('click', function() {
-        // 6.
-        clickCounter++;
-
-        var memberCount = $('.member').length;
-
-        if(memberCount == clickCounter) {
-            $('section.team').animate({
-                height: 0,
-                opacity: 0,
-                padding: 0
-            }, 1000, function() {
-                $(this).remove();
-            })
-        }
-        // end of 6.
-
-        $(this).parent().animate({
-            opacity: 0
-        }, 500, function() {
-            $(this).addClass('hidden');
-        });
-
-
-    });
-
-    // 4.
-    $('#follow').on('click', function() {
-        $('.instagram').toggleClass('small');
-    })
-
-    // 5.
-    $('#contact-form').on('submit', function(e) {
-        e.preventDefault();
-
-        $('section.contact').animate({
-            height: 0,
-            opacity: 0,
-            padding: 0
-        }, 1000, function() {
-            $(this).remove();
-        })
-
-    });
+    //Code
 
 });
